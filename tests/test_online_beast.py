194ac3488452f1357edfbac1b9b2c4d2e75cc161
Wytamma Wirth <wytamma.wirth@me.com>
2022-02-07 20:38:16 +1100

    first commit

---
+++ tests/test_online_beast.py
@@ -0,0 +1,30 @@
+import os
+from online_beast import __version__
+from typer.testing import CliRunner
+
+from online_beast.main import app
+
+runner = CliRunner()
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+def test_app():
+    result = runner.invoke(
+        app,
+        [
+            "data/testGTR.xml",
+            "data/samples.fasta",
+            "--state-file",
+            "data/testGTR.xml.state",
+            "--xml-output",
+            "testGTR.xml",
+            "--sate-output",
+            "testGTR.xml.state"
+        ],
+    )
+    assert result.exit_code == 0
+    os.remove('testGTR.xml')
+    os.remove('testGTR.xml.state')
